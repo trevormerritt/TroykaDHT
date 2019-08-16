e41a2506273c98c3e439d75110954d172aa66a49
Trevor Merritt <trevor.merritt@gmail.com>
2019-08-16 17:10:18 -0400

    Addresses Issue 3 on project this is forked from.
    Addds english translation

--- src/TroykaDHT.cpp
+++ src/TroykaDHT.cpp
@@ -4,8 +4,9 @@
 //  Arduino IDE:    Arduino-1.8.2
 //  Author:         Igor Dementiev
 //  Date:           Feb 22,2018
-//  Version:        v1.0
+//  Version:        v1.0.1
 //  by www.amperka.ru
+//  Modification:   Trevor Merritt - Add English Translation.
 /****************************************************************************/
 
 #include "TroykaDHT.h"
@@ -19,23 +20,32 @@ void DHT::begin() {
 }
 
 int8_t DHT::read() {
+    // data buffer
     // буффер данных
     uint8_t data[5];
+    // data bit
     // бит данных
     uint8_t dataBit;
+    // checksum
     // контрольнная сумма
     uint8_t checkSum;
     // очистка буффера
+    // flush the buffer
     for (int i = 0; i < 5; i++)
         data[i] = 0;
+    // push the bus to ground for 18ms
     // прижимаем шину к земле на 18 мс
     pinMode(_pin, OUTPUT);
     digitalWrite(_pin, LOW);
     delay(18);
+    // release the bus
     // отпускаем шину
     pinMode(_pin, INPUT_PULLUP);
+    // check the sensor response.
     // проверяем реакцию датчика
+    // for 20-40 us, the sensor clamps the bus to 0 and holds it for 80 us.
     // в течении 20-40 мкс датчик зажимает шину в 0 и держит 80 мкс 
+    // then releases for 80 us (generates a presence signal)
     // потом отпускает на 80 мкс (формирует сигнал присутствия)
     if (pulseInLength(_pin, HIGH, 40) == 0) {
         _state = DHT_ERROR_NO_REPLY;
@@ -52,8 +62,11 @@ int8_t DHT::read() {
         return _state;
     }
 
+    // read 40 bits of data
     // считываем 40 бит данных
+    // with the same start - clamping the bus for about 50us and a data bit.
     // с одинаковым стартом — зажатием шины около 50 мкс и битом данных
+    // the "sensor" releases the bus in ~27us if it wants to transmit
     // датчик «отпускает» шину на ~27мкс если хочет передать «0», или на ~70мкс если хочет передать «1»
     for (int i = 0; i < 40; i++) {
         pulseInLength(_pin, LOW, 50);
@@ -67,14 +80,17 @@ int8_t DHT::read() {
         }
     }
 
+    // calculate checksum
     // проверка контрольной суммы
     checkSum = data[0] + data[1] + data[2] + data[3];
+    // verify checksum
     // проверка контрольнной суммы
     if (data[4] != checkSum) {
         _state = DHT_ERROR_CHECKSUM;
         return _state;
     }
 
+    // write data
     // запись данных
     switch (_type) {
         case DHT11:
@@ -103,6 +119,7 @@ int8_t DHT::read() {
 
 unsigned long DHT::pulseInLength(uint8_t pin, bool state, unsigned long timeout) {
     unsigned long startMicros = micros();
+    // wait for the end of the current heart rate
     // ожидание окончания текущего пульса
     while (digitalRead(pin) == state) {
         if (micros() - startMicros > timeout)
